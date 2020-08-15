87aa9b8e3d04ad67c7581555704e5ca362185691
Jose <jocvegar@gmail.com>
2020-08-15 10:54:41 -0600

    updates candidate model tesT

--- test/models/candidate_test.rb
+++ test/models/candidate_test.rb
@@ -5,4 +5,21 @@ class CandidateTest < ActiveSupport::TestCase
 		candidate = build(:candidate)
 		assert candidate.valid?
 	end
+
+	test "should not without required fields" do
+		candidate1 = build(:candidate, email: "")
+		assert_not candidate1.valid?
+		candidate2 = build(:candidate, first_name: "")
+		assert_not candidate2.valid?
+		candidate3 = build(:candidate, last_name: "")
+		assert_not candidate3.valid?
+	end
+
+	test "should only allow selected status options" do
+		candidate = build(:candidate, status: "something")
+		assert candidate.invalid?
+		assert_match(candidate.errors.full_messages.to_sentence, "Status something is not a valid option")
+	end
+
+
 end
