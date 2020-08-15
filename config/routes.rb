a671f831fa95a659de7485d315ba207db178b806
Jose <jocvegar@gmail.com>
2020-08-15 13:15:08 -0600

    configures devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-
-  devise_for :users
 	root 'home#index'
 
 	scope :api, defaults: {format: :json} do
 		resources :candidates
+		devise_for :users, controllers: {sessions: 'sessions'}
+		devise_scope :user do
+		  get 'users/current', to: 'sessions#show'
+		end
 	end
 end
