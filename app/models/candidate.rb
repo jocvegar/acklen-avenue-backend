9f8d81291e18a694dacd65e67fc9b60a4939aeaf
Jose <jocvegar@gmail.com>
2020-08-15 10:27:01 -0600

    Adds model validations

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,4 +1,8 @@
 class Candidate < ApplicationRecord
 	extend FriendlyId
-	 friendly_id :email, use: :slugged
+	friendly_id :email, use: :slugged
+
+	validates_presence_of :first_name, :last_name, :email
+	validates :status, inclusion: { in: %w(review pass declined),
+		message: "%{value} is not a valid option", allow_nil: true }
 end
