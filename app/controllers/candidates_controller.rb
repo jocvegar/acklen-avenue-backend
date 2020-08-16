57b99b4799f1d0e617b8e35456df6368571f068c
Jose <jocvegar@gmail.com>
2020-08-16 11:57:52 -0600

    Adds create

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,10 +1,20 @@
 class CandidatesController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user!, except: [:create]
 	before_action :get_candidate, only: [:destroy]
 
 	def index
-		candidates = Candidate.all
+		candidates = Candidate.all.order(:first_name)
 		render json: candidates
+	end
+
+	def create
+		@candidate = Candidate.new(candidate_params)
+		@candidate.status = "review"
+		if @candidate.save
+			render json: { candidate: "Created succesfully." }.to_json, status: 201
+		else
+			render json: { errors: "Unable to create. #{@candidate.errors.full_messages}" }.to_json, status: 422
+		end
 	end
 
 	def destroy
@@ -21,5 +31,8 @@ class CandidatesController < ApplicationController
 		@candidate = Candidate.friendly.find(params[:id])
 	end
 
+	def candidate_params
+		params.require(:candidate).permit(:first_name, :last_name, :email)
+	end
 
 end
