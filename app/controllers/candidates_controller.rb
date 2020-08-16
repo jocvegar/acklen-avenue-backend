e406dd007a5bb2ea0171d2e7b868bd2fcddd7dbf
Jose <jocvegar@gmail.com>
2020-08-16 13:18:08 -0600

    add update action

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,6 +1,6 @@
 class CandidatesController < ApplicationController
-	before_action :authenticate_user!, except: [:create]
-	before_action :get_candidate, only: [:destroy]
+	before_action :authenticate_user!, except: [:create, :update]
+	before_action :get_candidate, only: [:update, :destroy]
 
 	def index
 		candidates = Candidate.all.order(:first_name)
@@ -14,6 +14,14 @@ class CandidatesController < ApplicationController
 			render json: { candidate: "Created succesfully." }.to_json, status: 201
 		else
 			render json: { errors: "Unable to create. #{@candidate.errors.full_messages}" }.to_json, status: 422
+		end
+	end
+
+	def update
+		if @candidate.update(candidate_update_params)
+			render json: { candidate: "Updated succesfully." }.to_json, status: 201
+		else
+			render json: { errors: "Unable to update. #{@candidate.errors.full_messages}" }.to_json, status: 422
 		end
 	end
 
@@ -33,6 +41,10 @@ class CandidatesController < ApplicationController
 
 	def candidate_params
 		params.require(:candidate).permit(:first_name, :last_name, :email)
+	end
+
+	def candidate_update_params
+		params.require(:candidate).permit(:status)
 	end
 
 end
