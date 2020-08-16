4f6ca451f59de59e86f0fbb84fde1fc26a825c4a
Jose <jocvegar@gmail.com>
2020-08-16 10:01:42 -0600

    Adds destroy method

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,8 +1,25 @@
 class CandidatesController < ApplicationController
 	before_action :authenticate_user!
+	before_action :get_candidate, only: [:destroy]
 
 	def index
 		candidates = Candidate.all
 		render json: candidates
 	end
+
+	def destroy
+		if @candidate.destroy
+			head :no_content
+		else
+			render json: { errors: @candidate.errors }.to_json, status: 422
+		end
+	end
+
+	private
+
+	def get_candidate
+		@candidate = Candidate.friendly.find(params[:id])
+	end
+
+
 end
