bce475337fc331b0def9863bd19d8803d16501dc
Jose <jocvegar@gmail.com>
2020-08-17 07:18:34 -0600

    adds tests

--- test/controllers/candidates_controller_test.rb
+++ test/controllers/candidates_controller_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class CandidatesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+	include Devise::Test::IntegrationHelpers
+
+	def setup
+		@user = create(:user)
+		@candidate = create(:candidate)
+	end
+
+	test "should get index" do
+		sign_in(@user)
+		get candidates_path
+		assert_response :success
+	end
+
+	test "should be able to create" do
+		sign_in(@user)
+		assert_difference "Candidate.count", 1, "should add candidate" do
+			post candidates_path,
+				params: { candidate:
+					{first_name: "name", last_name: "last", email: "email@email.com" } }
+		end
+	end
+
+	test "should be able to delete" do
+		sign_in(@user)
+		assert_difference "Candidate.count", -1, "should delete candidate" do
+			delete candidate_path(id: @candidate.slug)
+		end
+	end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "must be valid factory" do
+		user = build(:user)
+		assert user.valid?
+	end
+
+	test "must be have required fields" do
+		user1 = build(:user, email: "")
+		assert_not user1.valid?
+		user2 = build(:user, password: "")
+		assert_not user2.valid?
+	end
 end
